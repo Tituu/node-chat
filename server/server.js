993edc6d2565db6465d6cc56fa44f82b945edf42
Tituu <adrianhabda2@gmail.com>
2021-05-13 18:55:08 +0200

    fix

--- server/server.js
+++ server/server.js
@@ -71,7 +71,6 @@ io.on('connection', (socket) => {
 			io.to(user.room).emit('updateUserList', users.getUserList(user.room));
 			io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left.`));
 		}
-	}
 	});
 });
 
